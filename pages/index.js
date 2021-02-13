dd53b39c25c64bd2ba66ce2243923c8d0d09f193
FelipeMaeda <56806716+FelipeMaeda@users.noreply.github.com>
2021-02-13 09:45:05 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,7 @@ function Home() {
     return (
     <div>
         <h4>Home</h4>
-        <Contador />
-        <div>Teste</div>
     </div>
-    )
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
     )
 }
 
