7f0f61717db8a5f800b7e42c1721d4edee64dffa
FelipeMaeda <56806716+FelipeMaeda@users.noreply.github.com>
2021-02-13 19:58:21 -0300

    Create Content Dropdown

--- components/Dropdown/Section.js
+++ components/Dropdown/Section.js
@@ -1,25 +1,35 @@
 import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
-import { Context } from './Provider.js'
 
-// import { Container } from './styles';
+import { Context } from './Provider';
 
 export function DropdownSection({ option }) {
-    const { updateOptionProps, cachedId } = useContext(Context);
+  const { cachedId } = useContext(Context);
 
-    const { id, optionDimensions, optionCenterX, contentDimensions } = option;
+  const { id, optionCenterX, contentDimensions } = option;
 
-    const x = optionCenterX;
-    const isActive = cachedId === id;
+  const contentWidth = contentDimensions?.width || 0;
+  const x = optionCenterX - contentWidth / 2;
 
-    return (
-        <motion.div
-            className="dropdown-section"
-            animate={{
-                x,
-            }}    
-        >
-            <option.WrappedContent />
-        </motion.div>
-    )
+  const isActive = cachedId === id;
+
+  return (
+    <motion.div
+      className="dropdown-section"
+      initial={{
+        x,
+      }}
+      animate={{
+        x,
+        opacity: isActive ? 1 : 0,
+        pointerEvents: isActive ? 'unset' : 'none',
+      }}
+      transition={{
+        ease: 'easeOut',
+        opacity: { duration: 0.2 },
+      }}
+    >
+      <option.WrappedContent />
+    </motion.div>
+  );
 }
