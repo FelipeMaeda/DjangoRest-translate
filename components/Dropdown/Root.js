1a2dff61f8c1e62c5d19720fdcb9796143c6b334
FelipeMaeda <56806716+FelipeMaeda@users.noreply.github.com>
2021-02-14 17:42:01 -0300

    Edit dropdown menu

--- components/Dropdown/Root.js
+++ components/Dropdown/Root.js
@@ -1,21 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import { motion } from 'framer-motion';
 // import { Container } from './styles';
 import { DropdownSection } from './Section.js'
 import { Context } from './Provider.js';
 
 export function DropdownRoot() {
-    const { options } = useContext(Context);
+    const { options, cachedId, getOptionById, targetId } = useContext(Context);
+
+    const cachedOption = useMemo(() => getOptionById(cachedId), [
+        cachedId,
+        getOptionById,
+    ]);
+
+    let [width, height, x] = [0, 0, 0];
+    
+    if (cachedOption) {
+        const { optionCenterX, contentDimensions } = cachedOption;
+
+        width = contentDimensions ?.width;
+        height = contentDimensions ?.height;
+        x = optionCenterX - width /2;
+    }
+
+    const [hovering, setHovering] = useState(false);
+
+    const isActive = targetId !== null || hovering;
 
     return (
         <div className="dropdown-root">
-            <div>
-                <div className="dropdown-container">
+            <motion.div
+                className="dropdown-container"
+                animate={{
+                    x,
+                    width,
+                    height,
+                }}
+                onHoverStart={() => setHovering(true)}
+                onHoverEnd={() => setHovering(false)}
+            >
+                <motion.div
+                    animate={{
+                        x: -x,
+                    }}
+                >
                     {options.map((item) => (
                         <DropdownSection key={item.id} option={item} />
                     ))}
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </div>
     );
 }
